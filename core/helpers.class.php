eaee6e3b1c1849f18ce1c7fdb546a0c1d7f41469
unknown <JBARBE02@.emea.media.global.loc>
2011-04-13 10:01:57 +0200

    Displace the slugify method from helper to view . Usefull for slugify on the fly content

--- core/helpers.class.php
+++ core/helpers.class.php
@@ -2,24 +2,6 @@
 
 class Helpers{
 
-	//slufigy text
-	//Render : This is an URL with spécïal char!!!*^$^ù to this-is-an-url-with-special-char
-	public function slugify($text){
-		$text = preg_replace('~[^\\pL\d]+~u', '-', $text);
-		$text = trim($text, '-');
-
-		if (function_exists('iconv'))
-			$text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
-
-		$text = strtolower($text);
-		$text = preg_replace('~[^-\w]+~', '', $text);
-
-		if (empty($text))
-			return 'n-a';
-
-		return $text;
-	}
-
 	//upload file to specific location
 	public function upload($index,$destination,$maxsize=FALSE,$extensions=FALSE)
 	{
--- core/view.class.php
+++ core/view.class.php
@@ -1,57 +1,75 @@
 <?php
-	//View class
-	class View{
-		private $blocks = array();
+//View class
+class View{
+	private $blocks = array();
 
-		//Lauch the render of a block
-		//@param block string
-		//@return void
-		public function createBlock($block){
+	//Lauch the render of a block
+	//@param block string
+	//@return void
+	public function createBlock($block){
 
-			if(!isset($this->blocks[$block]))
-				$this->blocks[$block] = null;
+		if(!isset($this->blocks[$block]))
+			$this->blocks[$block] = null;
 
-			ob_start();
-		}
+		ob_start();
+	}
 
-		//End the render of a block and stock it
-		//@param block string
-		//@return void
-		public function endBlock($block){
+	//End the render of a block and stock it
+	//@param block string
+	//@return void
+	public function endBlock($block){
 
-			$value = ob_get_clean();
-			
-			$this->blocks[$block] .= $value;
-		}
+		$value = ob_get_clean();
+		
+		$this->blocks[$block] .= $value;
+	}
 
-		//Get the rendered block in the template
-		//@param block string
-		//@return string
-		public function getBlock($block){
-			return isset($this->blocks[$block]) ? $this->blocks[$block] : null;
-		}
+	//Get the rendered block in the template
+	//@param block string
+	//@return string
+	public function getBlock($block){
+		return isset($this->blocks[$block]) ? $this->blocks[$block] : null;
+	}
 
-		//Control is the asked block exists
-		//@param block string
-		//@return bool
-		public function issetBlock($block){
-			return isset($this->blocks[$block]) ? true : false;
-		}
+	//Control is the asked block exists
+	//@param block string
+	//@return bool
+	public function issetBlock($block){
+		return isset($this->blocks[$block]) ? true : false;
+	}
 
-		//Completely destroy the asked block
-		//@param block string
-		//@return void
-		public function destroyBlock($block){
-			unset($this->blocks[$block]);
-		}
+	//Completely destroy the asked block
+	//@param block string
+	//@return void
+	public function destroyBlock($block){
+		unset($this->blocks[$block]);
+	}
 
-		//Get a simple var (like titleVew) in the template
-		//@param block string
-		//@return ?
-		public function getVar($var){
-			global $theApp;
+	//Get a simple var (like titleVew) in the template
+	//@param block string
+	//@return ?
+	public function getVar($var){
+		global $theApp;
 
-			return $theApp->$var;
-		}
+		return $theApp->$var;
+	}
+
+	//slufigy text
+	//Render : This is an URL with spécïal char!!!*^$^ù to this-is-an-url-with-special-char
+	public function slugify($text){
+		$text = preg_replace('~[^\\pL\d]+~u', '-', $text);
+		$text = trim($text, '-');
+
+		if (function_exists('iconv'))
+			$text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+
+		$text = strtolower($text);
+		$text = preg_replace('~[^-\w]+~', '', $text);
+
+		if (empty($text))
+			return 'n-a';
+
+		return $text;
+	}
 }
 ?>
